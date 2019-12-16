560b964f6df6029425466a938fa35545cfea7f07
Daniel Saenz <disaenz2@gmail.com>
2019-12-15 20:44:56 -0700

    Adding the complete version of the SwiftCalculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -71,10 +71,23 @@ class ViewController: UIViewController {
     func isMathOperator(receivedOperator: String) -> Bool {
         return label.text != "" && receivedOperator != "C" && receivedOperator != "="
     }
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(appMovedToBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(appMovedToForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+
+    @objc func appMovedToForeground() {
+        print("Moved to foreground.")
+        startAccelerometerUpdates()
+    }
+    
+    @objc func appMovedToBackground() {
+        motionManager.stopAccelerometerUpdates()
+        print("Moved to background.")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -84,7 +97,35 @@ class ViewController: UIViewController {
     override open var shouldAutorotate: Bool {
         return false
     }
-        
+    
+    var motionManager = CMMotionManager()
+    
+    func startAccelerometerUpdates() {
+        motionManager.accelerometerUpdateInterval = 0.15
+                
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            if let acceleromoterData = data {
+                let yaw = self.sigmoid(acceleration: CGFloat(acceleromoterData.acceleration.z) * 5)
+                let pitch = self.sigmoid(acceleration: CGFloat(acceleromoterData.acceleration.x) * 5)
+                let roll = self.sigmoid(acceleration: CGFloat(acceleromoterData.acceleration.y) * 5)
+                
+                let newColor = UIColor(red: pitch, green: roll, blue: yaw, alpha: 1.0)
+                let complementaryColor = UIColor(red: 1 - pitch, green: 1 - roll, blue: 1 - yaw, alpha: 1.0)
+
+                for aButton in self.buttonCollection {
+                    aButton.backgroundColor = newColor
+                    aButton.setTitleColor(complementaryColor, for: .normal)
+                }
+                self.label.textColor = newColor
+            }
+        }
+    }
+ 
+    func sigmoid(acceleration: CGFloat) -> CGFloat {
+        return CGFloat(1/(1+exp(-1 * acceleration)))
+    }
+    
     @IBOutlet var buttonCollection: [roundButton]!
     
 }
+
